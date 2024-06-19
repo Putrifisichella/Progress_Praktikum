dbbd9b00c5dc3fb887aec151efe33304592cd297
Putrifisichella <2210631170096@student.unsika.ac.id>
2024-06-19 14:48:29 +0700

    progress praktikum pbw

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+
+    $hostname = "localhost";
+    $userDataBase = "root";
+    $passwordUser = "";
+    $dataBaseName = "pwb";
+
+    $koneksi = mysqli_connect($hostname,$userDataBase,$passwordUser,$dataBaseName) or die (mysqli_error());
+
+    // if($koneksi){
+    //     echo "berhasil koneksi";
+    // } else echo "gagal koneksi";
+
